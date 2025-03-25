e54f831a9fc5a5692762c69dccc78a3246e8427f
slowpoke-io <B094020008@gmail.com>
2025-03-25 19:06:47 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="container mx-auto p-5">TEST</div>;
+}
+
+export default App;
