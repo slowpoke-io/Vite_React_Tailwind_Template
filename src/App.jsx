466f6472ba2de2e39b3b689cbed2fa93fc66b44e
slowpoke-io <B094020008@gmail.com>
2025-03-25 19:17:51 +0800

    add components/

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="container mx-auto p-5">TEST</div>;
+  return (
+    <div className="container mx-auto p-5">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
